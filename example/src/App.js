9fd37e5a0b476447f30255f2801556bd432dbfd6
Stefan Diskic <disko998@gmail.com>
2021-05-06 03:10:32 +0200

    right actions

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,13 @@
-import {StyleSheet, SafeAreaView, FlatList, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  Alert,
+} from 'react-native';
 import React from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Swipeable from '../Swipeable';
 
 import data from './MOCK_DATA.json';
@@ -17,9 +25,24 @@ const App = () => {
 };
 
 const ListItem = ({first_name, last_name, email}) => {
+  const rightActions = [
+    {
+      key: 'Add',
+      color: '#8daef0',
+      onPress: () => Alert.alert(`Add ${first_name}`),
+      children: <FontAwesome name="user" size={30} color="#fff" />,
+    },
+    {
+      key: 'Remove',
+      color: '#f07067',
+      onPress: () => Alert.alert(`Remove ${first_name}`),
+      children: <FontAwesome name="trash-o" size={30} color="#fff" />,
+    },
+  ];
+
   return (
     <View style={{marginVertical: 5}}>
-      <Swipeable>
+      <Swipeable rightActions={rightActions}>
         <View style={styles.item}>
           <Text>{`${first_name} ${last_name}`}</Text>
           <Text>{email}</Text>
